d533beec2597f6a2928f86ae2ed835461e6213ba
raddop <radd3@icloud.com>
2025-11-20 16:55:06 +0000

    added engine_ai header and cpp files

---
+++ engine/engine_renderer.hpp
@@ -0,0 +1,12 @@
+#pragma once //ensures that the file is included only once in a single compilation
+#include <lib/SFML/Graphics.hpp>
+
+namespace Renderer {
+    void initialise(sf::RenderWindow &);
+    sf::RenderWindow &get_window();
+    
+    void shutdown();
+    void update(const float &);
+    void queue(const sf::Drawable *s);
+    void render();
+};
