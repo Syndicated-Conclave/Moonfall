283e18c15ecb7e70fc47c40aae8951c7afcf7f55
RebekkaEB <rebekkaeb@users.noreply.github.com>
2025-11-20 10:23:57 +0000

    Added resource copying

--- main.cpp
+++ main.cpp
@@ -2,14 +2,15 @@
 #include <SFML/Window.hpp>
 #include <iostream>
 
-int main(){
+int main()
+{
   sf::RenderWindow window(sf::VideoMode({800, 400}), "Moonfall");
 
   /*Adding custom font and styling for title*/
   sf::Font font;
-  font.loadFromFile("HUSKYSTA.ttf");
+  font.loadFromFile("resources/HUSKYSTA.otf");
   sf::Text title("Moonfall", font, 48);
-  title.setFillColor(sf::Color(255,255,224));
+  title.setFillColor(sf::Color(255, 255, 224));
   title.setPosition({300.f, 30.f});
 
   /*Creating gradient background colour*/
@@ -17,7 +18,7 @@ int main(){
   sky[0].position = sf::Vector2f(0, 0);
   sky[1].position = sf::Vector2f(window.getSize().x, 0);
   sky[2].position = sf::Vector2f(window.getSize().x, window.getSize().y);
-  sky[3].position = sf::Vector2f(0,window.getSize().y);
+  sky[3].position = sf::Vector2f(0, window.getSize().y);
   sf::Color deep(7, 7, 41);
   sf::Color light(24, 66, 75);
   sky[0].color = deep;
@@ -39,25 +40,28 @@ int main(){
   star.setPoint(8, sf::Vector2f(1, 3));
   star.setPoint(9, sf::Vector2f(6, 3));
   star.setFillColor(sf::Color(255, 253, 208));
-  
+
   /*Creating Menu Buttons*/
-  sf::RectangleShape guide({ 180.f, 35.f });
+  sf::RectangleShape guide({180.f, 35.f});
   guide.setPosition(300.f, 130.f);
-  sf::RectangleShape play({ 180.f, 35.f });
+  sf::RectangleShape play({180.f, 35.f});
   play.setPosition(300.f, 180.f);
-  sf::RectangleShape controls({ 180.f, 35.f });
+  sf::RectangleShape controls({180.f, 35.f});
   controls.setPosition(300.f, 230.f);
-  sf::RectangleShape exit({ 180.f, 35.f });
+  sf::RectangleShape exit({180.f, 35.f});
   exit.setPosition(300.f, 280.f);
   guide.setFillColor(sf::Color(255, 255, 224));
   play.setFillColor(sf::Color(255, 255, 224));
   controls.setFillColor(sf::Color(255, 255, 224));
   exit.setFillColor(sf::Color(255, 255, 224));
 
-  while (window.isOpen()){
-      sf::Event event;
-      while (window.pollEvent(event)){
-      if (event.type == sf::Event::Closed){
+  while (window.isOpen())
+  {
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+      {
         window.close();
       }
     }
@@ -65,13 +69,13 @@ int main(){
     window.clear();
     window.draw(sky);
     window.draw(title);
-    star.setPosition({ 299.f, 87.f });
+    star.setPosition({299.f, 87.f});
     window.draw(star);
-    star.setPosition({ 360.f, 87.f });
+    star.setPosition({360.f, 87.f});
     window.draw(star);
-    star.setPosition({ 409.f, 87.f });
+    star.setPosition({409.f, 87.f});
     window.draw(star);
-    star.setPosition({ 464.f, 87.f });
+    star.setPosition({464.f, 87.f});
     window.draw(star);
     window.draw(guide);
     window.draw(play);
