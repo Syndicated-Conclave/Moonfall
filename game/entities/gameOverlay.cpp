4ba3df58ccef7e22580cd5072a0d4727aa294454
raddop <radd3@icloud.com>
2025-11-25 15:22:45 +0000

    gameOverlay files

---
+++ game/entities/gameOverlay.cpp
@@ -0,0 +1,32 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
+
+namespace gameOverlay
+{
+    class UI()
+    {
+        /*Adding custom font and styling for star dust counter
+        sf::Font UIfont;
+        UIfont.loadFromFile("resources/HUSKYSTA.otf");
+        sf::Text dustCounter("Dust Collected: ", UIfont, 48);
+        dustCounter.setFillColor(sf::Color(255, 255, 224));
+        dustCounter.setPosition({20.f, 10.f});
+        */
+
+         sf::Vector2f posX;
+         sf::Vector2f posY;
+
+         sf::Font font;
+         sf::Text text;
+         sf::Color color;
+
+    }
+
+    class DawnTimerUI::UI()
+    {
+
+    }
+
+};
+
