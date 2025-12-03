056fba9c89b982a06fd630f446facc22c0c25829
TheSquibblingJabberwocky <cassiehassell@hotmail.com>
2025-12-03 16:26:42 +0000

    Menu update

--- game/scenes/menu.cpp
+++ game/scenes/menu.cpp
@@ -1,93 +1,155 @@
 /*
-
-FROM THE LABS:
-
-
+#include "menu.hpp"
+#include "levelMenu.hpp"
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 #include <iostream>
 
-//usings for our level creator, game parameters and game system
-using LS = LevelSystem;
-using param = Parameters;
-using GS = GameSystem;
+namespace Game
+{
+    namespace Scenes
+    {
+        void Menu::mainMenu(sf::RenderWindow& window, Engine::EntityManager& ecm)
+        {
+            // Adding custom font and styling for title
+            sf::Font font;
+            font.loadFromFile("HUSKYSTA.otf");
+            sf::Text title("Moonfall", font, 88);
+            title.setFillColor(sf::Color(255, 255, 224));
+            title.setPosition({ 600.f, 100.f });
 
-//shared pointers
-std::shared_ptr<Scene> Scenes::menu;
-std::shared_ptr<Scene> Scenes::level;
+            // Creating gradient background colour
+            sf::VertexArray sky(sf::Quads, 4);
+            sky[0].position = sf::Vector2f(0, 0);
+            sky[1].position = sf::Vector2f(window.getSize().x, 0);
+            sky[2].position = sf::Vector2f(window.getSize().x, window.getSize().y);
+            sky[3].position = sf::Vector2f(0, window.getSize().y);
+            sf::Color deep(7, 7, 41);
+            sf::Color light(24, 66, 75);
+            sky[0].color = deep;
+            sky[1].color = deep;
+            sky[3].color = light;
+            sky[2].color = light;
 
-#pragma region MENU SCENE
-void MenuScene::update(const float &dt) {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
-        gs::set_active_scene(Scenes::level);
+            // Creating star object
+            sf::ConvexShape star;
+            star.setPointCount(10);
+            star.setPoint(0, sf::Vector2f(8, -2));
+            star.setPoint(1, sf::Vector2f(10, 3));
+            star.setPoint(2, sf::Vector2f(15, 3));
+            star.setPoint(3, sf::Vector2f(11, 6));
+            star.setPoint(4, sf::Vector2f(12, 11));
+            star.setPoint(5, sf::Vector2f(8, 8));
+            star.setPoint(6, sf::Vector2f(4, 11));
+            star.setPoint(7, sf::Vector2f(5, 6));
+            star.setPoint(8, sf::Vector2f(1, 3));
+            star.setPoint(9, sf::Vector2f(6, 3));
+            star.setFillColor(sf::Color(255, 253, 208));
+            star.scale({ 1.75f,1.75f });
+
+            // Creating Menu Buttons
+            sf::RectangleShape guide({ 380.f, 65.f });
+            guide.setPosition(600.f, 300.f);
+            sf::RectangleShape play({ 380.f, 65.f });
+            play.setPosition(600.f, 400.f);
+            sf::RectangleShape controls({ 380.f, 65.f });
+            controls.setPosition(600.f, 500.f);
+            sf::RectangleShape exit({ 380.f, 65.f });
+            exit.setPosition(600.f, 600.f);
+            guide.setFillColor(sf::Color(255, 255, 224));
+            play.setFillColor(sf::Color(255, 255, 224));
+            controls.setFillColor(sf::Color(255, 255, 224));
+            exit.setFillColor(sf::Color(255, 255, 224));
+
+            //Making labels for my buttons
+            sf::Text Guide("Guide", font);
+            Guide.setCharacterSize(40);
+            Guide.setFillColor(sf::Color(11, 27, 44));
+            Guide.setPosition(730.f, 300.f);
+
+            sf::Text Play("Play", font);
+            Play.setCharacterSize(40);
+            Play.setFillColor(sf::Color(11, 27, 44));
+            Play.setPosition(740.f, 400.f);
+
+
+            sf::Text Controls("Controls", font);
+            Controls.setCharacterSize(40);
+            Controls.setFillColor(sf::Color(11, 27, 44));
+            Controls.setPosition(710.f, 500.f);
+
+            sf::Text Exit("Exit", font);
+            Exit.setCharacterSize(40);
+            Exit.setFillColor(sf::Color(11, 27, 44));
+            Exit.setPosition(740.f, 600.f);
+
+
+            window.clear();
+            window.draw(sky);
+            window.draw(title);
+            star.setPosition({ 590.f, 200.f });
+            window.draw(star);
+            star.setPosition({ 620.f, 200.f });
+            window.draw(star);
+            star.setPosition({ 720.f, 200.f });
+            window.draw(star);
+            star.setPosition({ 750.f, 200.f });
+            window.draw(star);
+            star.setPosition({ 780.f, 200.f });
+            window.draw(star);
+            star.setPosition({ 810.f, 200.f });
+            window.draw(star);
+            star.setPosition({ 840.f, 200.f });
+            window.draw(star);
+            star.setPosition({ 870.f, 200.f });
+            window.draw(star);
+            star.setPosition({ 900.f, 200.f });
+            window.draw(star);
+            star.setPosition({ 930.f, 200.f });
+            window.draw(star);
+            star.setPosition({ 960.f, 200.f });
+            window.draw(star);
+            window.draw(guide);
+            window.draw(Guide);
+            window.draw(play);
+            window.draw(Play);
+            window.draw(controls);
+            window.draw(Controls);
+            window.draw(exit);
+            window.draw(Exit);
+            window.display();
+
+
+
+        }
+            void Menu::handleEvent(sf::RenderWindow & window, sf::Event & event)
+            {
+                if (event.type == sf::Event::MouseButtonPressed)
+                {
+                    sf::Vector2f mouse = window.mapPixelToCoords(
+                        sf::Mouse::getPosition(window)
+                    );
+                    if (exit.getGlobalBounds().contains(mouse))
+                    {
+                        window.close();
+                    }
+                    if (guide.getGlobalBounds().contains(mouse))
+                    {
+                        std::cout << "Guide" << std::endl;
+                    }
+                    if (play.getGlobalBounds().contains(mouse))
+                    {
+                        std::cout << "Play" << std::endl;
+
+                        levelMenu.chooseLevel(window, ecm);
+                    }
+                    if (controls.getGlobalBounds().contains(mouse))
+                    {
+                        std::cout << "Controls" << std::endl;
+                    }
+
+            }
+
+
     }
-    Scene::update(dt);
-}
-
-void MenuScene::render() {
-    Renderer::queue(&_text);
-    Scene::render();
-}
-
-void MenuScene::load() {
-    //TODO Add in our font here
-    //_font.loadFromFile("resources/fonts/RobotoMono-Regular.ttf");
-    _text.setFont(_font);
-    _text.setCharacterSize(60);
-    _text.setString("Moonfall level 1 Test\n Press Space to start");
-}
-
-void MenuScene::unload(){}
-
-#pragma endregion
-
-#pragma region LEVEL SCENE
-void LevelScene::load(){
-    _load_level(param::level_1);
-}
-
-void LevelScene::_load_level(const std::string &file_path){
-    ls::load_level(file_path,param::tile_size);
-
-
-    _player = make_entity();
-    _player->set_position(ls::get_start_position());
-
-    std::shared_ptr<ShapeComponent> shape = _player->add_component<ShapeComponent>();
-    shape->set_shape<sf::RectangleShape>(sf::Vector2f(param::player_size[0],param::player_size[1]));
-    shape->get_shape().setFillColor(sf::Color::Yellow);
-    shape->get_shape().setOrigin(sf::Vector2f(param::player_size[0]/2.f,param::player_size[1]/2.f));
-    std::shared_ptr<PlayerPhysicsComponent> cmp = _player->add_component<PlayerPhysicsComponent>(sf::Vector2f(param::player_size[0],param::player_size[1]));
-        cmp->create_capsule_shape(sf::Vector2f(param::player_size[0],param::player_size[1]),
-        param::player_weight,param::player_friction,param::player_restitution);
-
-    std::vector<std::vector<sf::Vector2i>> wall_groups = ls::get_groups(ls::WALL);
-    for (const std::vector<sf::Vector2i> &walls : wall_groups) {
-        _walls.push_back(make_entity());
-        _walls.back()->add_component<PlatformComponent>(walls);
-    }
-
-}
-
-void LevelScene::update(const float &dt){
-    Scene::update(dt);
-    _entities.update(dt);
-    if(ls::get_tile_at(_player->get_position()) == ls::END){
-        unload();
-        _load_level(param::level_2);
-    }
-
-}
-
-void LevelScene::render(){
-    ls::render(Renderer::get_window());
-    Scene::render();
-    _entities.render();
-}
-
-void LevelScene::unload(){
-    Scene::unload();
-    _player.reset();
-    _walls.clear();
-}
-#pragma endregion
-
-*/
+} */
