424de98bebb959602a9f5e44797a64d308cdc207
XxAxNxAxDxU <inkosimack@gmail.com>
2025-12-06 19:34:45 +0000

    Adjust stardust checkpoints in level.cpp
    Updated stardust checkpoint positions in level.

--- game/scenes/level.cpp
+++ game/scenes/level.cpp
@@ -273,13 +273,13 @@ namespace Game
             cityscape.addBuilding(110, 560);
             cityscape.addBuilding(120, 520);
             cityscape.addBuilding(80, 440);
-            cityscape.addBuilding(100, 360);
+            cityscape.addBuilding(100, 360); // stardust checkpoint at 7860
             cityscape.addBuilding(120, 280);
-            cityscape.addBuilding(220, 200); // stardust checkpoint at 8200
+            cityscape.addBuilding(220, 200); 
 
             allStardust.push_back(Game::Entities::Stardust(window, ecm, { 2640, 300 }, 1));
             allStardust.push_back(Game::Entities::Stardust(window, ecm, { 5460, 300 }, 2));
-            allStardust.push_back(Game::Entities::Stardust(window, ecm, { 8200, 300 }, 3));
+            allStardust.push_back(Game::Entities::Stardust(window, ecm, { 7860, 300 }, 3));
 
             return 15;
         }
